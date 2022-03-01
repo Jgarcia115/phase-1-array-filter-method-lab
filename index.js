e63173222d2eb567329189d39d230becee42bdca
Jonathan <jono_95@yahoo.com>
2022-03-01 18:55:08 -0500

    commit tree 35285e2bcbfbbd5a11565c3bc8b595c42584a7f5

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching(collection, name) {
+    return collection.filter(function (names) {
+        return names.toLowerCase() === name.toLowerCase();
+    });
+}
+
+function fuzzyMatch(collection, name) {
+    return collection.filter(function (names) {
+        return names.charAt(0) === name.charAt(0);
+    });
+}
+
+function matchName(collection, string) {
+    return collection.filter(names => (names.name === string))
+}
